12bb5a6e2165f8a1faad050808f05b7dd7b22ff1
thomasaiman <32423251+thomasaiman@users.noreply.github.com>
2018-02-13 05:33:32 -0600

    installer type comment

--- todotxt.net/tools/chocolateyinstall.ps1
+++ todotxt.net/tools/chocolateyinstall.ps1
@@ -15,7 +15,7 @@ $packageArgs = @{
   checksumType  = 'sha256'
   checksum64    = '7D2ACADE56164EF7E0396F0293EB8275554799BCFB2209C51EDD4622F9BA21B0'
   checksumType64= 'sha256'
-
+  #The installer is made with InnoSetup
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
