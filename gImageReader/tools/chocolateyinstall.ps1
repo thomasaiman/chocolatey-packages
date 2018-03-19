67b673a57105d89dd19143fb259a2ff86baf46a2
thomasaiman <32423251+thomasaiman@users.noreply.github.com>
2018-03-19 17:08:01 -0500

    add gImageReader package
    still has uninstall bugs + extra files

---
+++ gImageReader/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/manisandro/gImageReader/releases/download/v3.2.99/gImageReader_3.2.99_qt5_i686.exe'
+$url64      = 'https://github.com/manisandro/gImageReader/releases/download/v3.2.99/gImageReader_3.2.99_qt5_x86_64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'gImageReader*'
+
+  checksum      = '905E6C3F6803C46858A503AD1BB763E41C7ACE2E038ABC095067656AF0725D58'
+  checksumType  = 'sha256'
+  checksum64    = '302F59859F9E8CEF5251094219C31658DD6F4A3E9EA39E396F87B1331084AB18'
+  checksumType64= 'sha256'
+  
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
